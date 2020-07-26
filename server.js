11343de046cc3bc8c0459844dfedaccc765253dc
Ali Kariapper <kariappa@mcmaster.ca>
2020-07-26 19:10:50 -0400

    Added basic endpoints

---
+++ server.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const { response } = require("express");
+
+const app = express();
+app.use(express.json());
+
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'ali',
+            email: 'ali@hotmail.com',
+            password: 'react',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '456',
+            name: 'bob',
+            email: 'bob@hotmail.com',
+            password: 'node',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+// app.use(express.urlencoded({extended: false}));
+
+
+app.get('/', (req, res) => {
+    res.send(database.users)
+})
+
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('success')
+    }
+    else {
+        res.status(400).json('error');
+    }
+
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body
+    database.users.push(
+        {
+            id: '125',
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+        }
+    )
+    res.json(database.users[database.users.length - 1])
+})
+
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true
+            return res.json(user);
+        }
+
+    })
+
+    if (!found) {
+        res.status(400).json("not found")
+    }
+})
+
+
+
+app.post('/image', (req,res) => {
+    const { id } = req.body
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true
+            user.entries++
+            return res.json(user.entries);
+        }
+
+    })
+
+        if (!found) {
+        res.status(400).json("not found")
+    }
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log("app is running on port 3000")
+})
+
+/*
+/ --> res = this is working
+/signin -> POST success/fail (if already cerated why POST, because dont want to be using a query string)
+/register -> POST user
+/profile/:userId --> GET user
+/image --> PUT --> user
+*/
